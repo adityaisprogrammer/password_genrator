57353b914096fd886a2bf0fd591c07fdd9be4e26
Aditya <ajit.alkem79@gmail.com>
2022-03-20 17:20:43 +0530

    hdh

---
+++ password genrator.py
@@ -0,0 +1,23 @@
+import random
+
+list_int = [1,2,3,4,5,6,7,8, 9,10,11,12]
+lsit = [1,2,3,4,5,6,7,8, 9,10,11,12]
+list2 = ["A", "b", "c", "d","e","f", "g", "h", "i", "j", "k","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+list3 = ["A", "b", "c", "d","e","f", "g", "h", "i", "j", "k","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+list4 = ["A", "b", "c", "d","e","f", "g", "h", "i", "j", "k","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+list5 = ["A", "b", "c", "d","e","f", "g", "h", "i", "j", "k","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+list6 = ["A", "b", "c", "d","e","f", "g", "h", "i", "j", "k","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+list7 = ["A", "b", "c", "d","e","f", "g", "h", "i", "j", "k","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+special_charcters = ("@")
+
+
+rand2 = random.choice(list2)
+rand3 = random.choice(list3)
+rand4 = random.choice(list4)
+rand5 = random.choice(list5)
+rand6 = random.choice(list6)
+rand7 = random.choice(list7)
+rand8 = random.choice(special_charcters)
+rand1 = random.choice(list_int)
+rand9 = random.choice(lsit)
+print(rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand1,rand9)
